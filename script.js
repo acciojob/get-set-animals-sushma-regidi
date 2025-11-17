15ee8253eeb5510942b8adb91f9861d8d462eb9e
sushma-regidi <regidisushma@gmail.com>
2025-11-18 01:03:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,42 +1,40 @@
-// Base Animal class
 class Animal {
-  constructor(species) {
-    this._species = species; // store species privately
-  }
+    constructor(species) {
+        this._species = species;
+    }
 
-  // Getter for species
-  get species() {
-    return this._species;
-  }
+    get species() {
+        return this._species;
+    }
 
-  // Method to make sound
-  makeSound() {
-    console.log(`The ${this._species} makes a sound`);
-  }
+    makeSound() {
+        console.log(`The ${this._species} makes a sound`);
+    }
 }
 
-// Cat class extending Animal
 class Cat extends Animal {
-  // Cat specific method
-  purr() {
-    console.log("purr");
-  }
+    constructor(species) {
+        super(species);
+    }
+
+    purr() {
+        console.log("purr");
+    }
 }
 
-// Dog class extending Animal
 class Dog extends Animal {
-  // Dog specific method
-  bark() {
-    console.log("woof");
-  }
+    constructor(species) {
+        super(species);
+    }
+
+    bark() {
+        console.log("woof");
+    }
 }
 
-// Example usage:
-// const myCat = new Cat("Siamese");
-// myCat.makeSound(); // The Siamese makes a sound
-// myCat.purr();      // purr
+// Export for Cypress if required
+if (typeof module !== "undefined") {
+    module.exports = { Animal, Cat, Dog };
+}
 
-// const myDog = new Dog("Golden Retriever");
-// myDog.makeSound(); // The Golden Retriever makes a sound
-// myDog.bark();      // woof
 
